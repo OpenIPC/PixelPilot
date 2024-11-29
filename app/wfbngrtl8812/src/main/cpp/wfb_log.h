e21dfbf83dc44464b0b6051eea60de9f3119943e
Ihor Ivlev <ivlev.igor@gmail.com>
2024-11-29 22:15:35 +0100

    Log improvement (#26)
    Log lost packets and errors from WFB-NG

---
+++ app/wfbngrtl8812/src/main/cpp/wfb_log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <android/log.h>
+#include <cstdarg>
+
+#define WFB_ERR(...) do{ __android_log_print( ANDROID_LOG_ERROR, "wfb-ng", __VA_ARGS__); } while(0)
+#define WFB_INFO(...) do{ __android_log_print( ANDROID_LOG_INFO, "wfb-ng", __VA_ARGS__); } while(0)
+
+#define WFB_DBG(...) (void(0))
+
+#define ANDROID_IPC_MSG(...) do{ __android_log_print( ANDROID_LOG_INFO, "wfb-ng", __VA_ARGS__); } while(0)
+#define IPC_MSG(...) do{ __android_log_print( ANDROID_LOG_INFO, "wfb-ng", __VA_ARGS__); } while(0)
+#define IPC_MSG_SEND() (void)0
