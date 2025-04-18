a66ffa3c69e0596441c1e4449045a27afda45c24
Ihor Ivlev <ivlev.igor@gmail.com>
2025-04-18 18:33:28 +0200

    Revert "Fix BufferedPacketQueue + test"
    This reverts commit 43fc056f38cc52ffc9d10f024531aca4ed2b4ffa.

--- app/videonative/src/main/cpp/BufferedPacketQueue.h
+++ app/videonative/src/main/cpp/BufferedPacketQueue.h
@@ -1,15 +1,9 @@
-#if defined(__ANDROID__) || defined(__ANDROID_API__)
 #include <android/log.h>
-#else
-#include <cassert>
-#include <cstdio>
-#endif
 #include <algorithm>
 #include <cstdarg>
 #include <cstddef>
 #include <cstdint>
 #include <limits>
-
 #include <unordered_map>
 #include <vector>
 
@@ -21,8 +15,7 @@ constexpr size_t MAX_BUFFER_SIZE = 50;
 constexpr size_t MONOTONIC_THRESHOLD = 40;
 
 // Type definition for sequence numbers
-using SeqType   = uint16_t;
-using SignedSeq = std::make_signed_t<SeqType>;
+using SeqType = uint16_t;
 
 /**
  * @brief BufferedPacketQueue class handles packet processing with sequence numbers,
@@ -111,10 +104,7 @@ class BufferedPacketQueue
      * @param currPacketIdx Sequence index of the incoming packet.
      * @return True if it's the next expected packet; otherwise, false.
      */
-    bool isNextExpectedPacket(SeqType currPacketIdx) const
-    {
-        return currPacketIdx == static_cast<SeqType>(mLastPacketIdx + 1);
-    }
+    bool isNextExpectedPacket(SeqType currPacketIdx) const { return currPacketIdx == mLastPacketIdx + 1; }
 
     /**
      * @brief Processes an in-order packet by invoking the callback and updating state.
@@ -322,20 +312,11 @@ class BufferedPacketQueue
      */
     void logDebug(const char* format, ...) const
     {
-#if defined(__ANDROID__) || defined(__ANDROID_API__)
         return;
         va_list args;
         va_start(args, format);
         __android_log_vprint(ANDROID_LOG_DEBUG, BUFFERED_QUEUE_LOG_TAG, format, args);
         va_end(args);
-#else
-        // Fallback to standard output for non-Android platforms
-        va_list args;
-        va_start(args, format);
-        vfprintf(stderr, format, args);
-        va_end(args);
-        fprintf(stderr, "\n");
-#endif
     }
 
     /**
@@ -345,18 +326,9 @@ class BufferedPacketQueue
      */
     void logWarning(const char* format, ...) const
     {
-#if defined(__ANDROID__) || defined(__ANDROID_API__)
         va_list args;
         va_start(args, format);
         __android_log_vprint(ANDROID_LOG_WARN, BUFFERED_QUEUE_LOG_TAG, format, args);
         va_end(args);
-#else
-        // Fallback to standard output for non-Android platforms
-        va_list args;
-        va_start(args, format);
-        vfprintf(stderr, format, args);
-        va_end(args);
-        fprintf(stderr, "\n");
-#endif
     }
 };
